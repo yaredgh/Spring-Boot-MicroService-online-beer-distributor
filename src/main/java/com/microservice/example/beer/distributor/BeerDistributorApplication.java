814f02aa4827293e677316c9c8de1eeb98e5f314
Yaredg <46734446+Yaredg@users.noreply.github.com>
2020-03-03 19:23:43 -0600

    initial project setUp.

---
+++ src/main/java/com/microservice/example/beer/distributor/BeerDistributorApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.example.beer.distributor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerDistributorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerDistributorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservice/example/beer/distributor/BeerDistributorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.example.beer.distributor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerDistributorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
